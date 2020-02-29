86a842ba6eaf7f4c40fea50eebb1f77af5a57498
ekayiigirwa <ekayigirwa@agnesscott.edu>
2020-02-29 15:14:33 -0500

    added views to create posts

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
-
+#pk is a unique id for each post model we have
+#post/<int:pk>/ specifies a URL pattern – we will explain it for you:
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
 
